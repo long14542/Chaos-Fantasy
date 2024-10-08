b4b8bbffa8169e7ee2f528b393a805855a2536a9
Sondmani <Szonny@proton.me>
2024-10-08 15:36:38 +0700

    (closes #4) Added Enemy Movement System - EnemyMovement, enemy now will move towards player

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}
