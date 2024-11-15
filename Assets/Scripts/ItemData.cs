41e430e3aca96e677bdbb7f40acd6e278431e300
long14542 <121614259+long14542@users.noreply.github.com>
2024-11-15 21:08:05 +0700

    add sword class

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-// Base class for weaponData and itemData
 public abstract class ItemData : ScriptableObject
 {
-    public Sprite icon;
+    public Sprite icon; 
     public int maxLevel;
     public string itemName;
 }
+
